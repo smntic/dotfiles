040f4cddf96160f35aa5f04fbff72b592dc6f476
Simon Ashton <simon@smntic.dev>
2025-11-08 16:27:22 -0800

    Add simon to groups

--- config/users/users.nix
+++ config/users/users.nix
@@ -4,7 +4,7 @@
   users.users = {
     "simon" = {
       isNormalUser = true;
-      extraGroups = [ "wheel" "networkmanager" "scanner" "lp" ];
+      extraGroups = [ "wheel" "networkmanager" "libvirtd" "kvm" "scanner" "lp" ];
       hashedPassword = "$6$rounds=4096$l2kpean.aM0buyhc$jBdmzGn7KNDLVFNIohinggosVdmUYVAxyVp.7XC5TbdcMIWkuD6BonSitTT15QGS0isPF0McCupayZEcomS2W/";
     };
   };
