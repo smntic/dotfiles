0b2a28d3f2589e4b1f4f2ca0189181fac2a7b8d5
Simon Ashton <simon@smntic.dev>
2026-02-01 10:59:33 -0800

    Sync

--- config/boot/boot.nix
+++ config/boot/boot.nix
@@ -17,5 +17,9 @@
   # unfortunately, this seems required, for now. maybe try to remove later.
   boot.kernelParams = [
     "amdgpu.dcdebugmask=0x10"
+    "snd_hda_intel.dmic_detect=0" # mic issue (not not working AT ALL)
+    # fix mic clicking sound
+    "snd_hda_intel.power_save=0"
+    "snd_hda_intel.power_save_controller=N"
   ];
 }
